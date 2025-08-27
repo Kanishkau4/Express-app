02a98e06947e6617bfb806f12a982bd61329c022
Kanishkau4 <kanishkau22@gmail.com>
2025-08-27 18:02:44 +0530

    Remove dist dictory and its contents; update .gitignore to exclude dist

---
+++ src/common/constants/errors.constants.ts
@@ -0,0 +1,14 @@
+export const ERRORS = {
+    BAD_REQUEST: {
+        key: "BAD_REQUEST",
+        message: "Invalid syntax or missing parameters"
+    },
+    UNAUTHORIZED: {
+        key: "UNAUTHORIZED",
+        message: "You are not allow to access this resource"
+    },
+    LOGIN_FAILED: {
+        key: "LOGIN_FAILED",
+        message: "Invalid username or password"
+    }
+}
